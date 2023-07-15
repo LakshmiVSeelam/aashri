573b408d49d407bfa26666579a585eeaeda0d1bc
LakshmiSeelam <lakshmi.seelam@avidbots.com>
2023-07-15 15:40:36 +0530

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,13 +9,13 @@ import { HomeComponent } from './pages/home.page/home.component';
 import { MediaComponent } from './pages/media.page/media.component';
 
 const routes: Routes = [
-  { path: '',   redirectTo: 'home', pathMatch: 'full' },
-  {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'media', component: MediaComponent},
-  {path: 'gallery', component: GalleryPageComponent},
-  {path: 'donate', component: DonateComponent},
-  {path: 'contact', component: ContactComponent},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  {path: '/home', component: HomeComponent},
+  {path: '/about', component: AboutComponent},
+  {path: '/media', component: MediaComponent},
+  {path: '/gallery', component: GalleryPageComponent},
+  {path: '/donate', component: DonateComponent},
+  {path: '/contact', component: ContactComponent},
   // { path: '**', component: PageNotFoundComponent }
 ];
 
